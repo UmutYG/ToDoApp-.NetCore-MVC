265eb444e3f8fefa3df0136a7374988fe5bb8abe
Umut Yılmaz Gündüz <umutgndz13@gmail.com>
2022-10-02 12:11:00 +0300

    Added webApi Support With webapi layer

---
+++ todoapp.webapi/Controllers/TasksController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using todoapp.business.Abstract;
+
+namespace todoapp.webapi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        private ITaskService _taskService;
+        public TasksController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+        [HttpGet]
+        public IActionResult GetTasks()
+        {
+            var tasks = _taskService.GetAll();
+            return Ok(tasks);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetTask(int id)
+        {
+            var task = _taskService.GetById(id);
+            if(task == null)
+            {
+                return NotFound(); // 404
+            }
+            return Ok(task); // 200
+        }
+    }
+
+    
+}
